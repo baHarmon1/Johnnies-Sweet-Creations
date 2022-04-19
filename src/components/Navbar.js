0ebc14932898f22c97db8b97fa2cc2a1af34253a
Brycen Harmon <brycenharmon@gmail.com>
2022-04-19 16:44:55 -0500

    added css cdn for bootstrap

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button'
 
 function Navbar() {
   return (
     <div>
     <p>Navbar</p>
-    
+    <Button variant="outline-success">Success</Button>
     </div>
   )
 }
